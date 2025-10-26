a5e484da17e67242ba4ab98ce3db2452a1e6f814
Gerardo <gerardoalonso.martinezmonge@gmail.com>
2025-10-26 14:43:29 -0600

    feat, added components, input, Footer, Header, pagination, jumbotron

--- src/shop/home/HomePage.tsx
+++ src/shop/home/HomePage.tsx
@@ -1,6 +1,14 @@
+import { CustomPagination } from "@/components/custom/CustomPagination"
+import { CustomJumbotron } from "../components/CustomJumbotron"
 
 export const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <>
+      <CustomJumbotron
+        title="Todos los productos"
+      />
+
+      <CustomPagination totalPages={7}/>
+    </>
   )
 }
